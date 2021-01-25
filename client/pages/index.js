d6123180aa759ffa0d6332ba9cb60c3a2657e548
ryand67 <rwdevenney@gmail.com>
2021-01-25 11:10:46 -0500

    different fonts

--- client/pages/index.js
+++ client/pages/index.js
@@ -8,13 +8,13 @@ export default function Home() {
         <title>Matt Moore</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Bodoni+Moda:wght@400;500;600;700&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet"></link>
+        <link href="https://fonts.googleapis.com/css2?family=Imbue&display=swap" rel="stylesheet"></link>
+        <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet" />
       </Head>
 
       <main className={styles.hero}>
         <h1 className={styles.siteHeader}>MATT MOORE</h1>
-        {/* <hr className={styles.heroHr} /> */}
+        <hr className={styles.heroHr} />
         <p className={styles.heroDesc}>Singer, Songwriter, Musician</p>
         <button className={styles.heroButton}>About Me</button>
       </main>
